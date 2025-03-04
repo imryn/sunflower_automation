0c151733371132ecdd64d8d4fef1ee391da08b7f
imryn <noyimry@gmail.com>
2025-03-04 18:36:34 +0200

    add docs to helper.ts and add classes to fixture

--- fixture.ts
+++ fixture.ts
@@ -1,13 +1,54 @@
 import { test as base } from '@playwright/test';
 import { HomePage } from './pages/homePage'
+import { RegistrationPage } from './pages/registrationPage';
+import { DigitalDownloadsPage } from './pages/digitalDownloadsPage';
+import { ShoppingCartPage } from './pages/shoppingCartPage';
 
 export type TestOptions = {
     homePage: HomePage
+    registrationPage: RegistrationPage
+    digitalDownloadsPage: DigitalDownloadsPage
+    shoppingCartPage: ShoppingCartPage
 }
 
+
+
 export const test = base.extend<TestOptions>({
+    /**
+     * Provides an instance of HomePage for tests.
+     * @param page - The Playwright page instance.
+     * @param use - The function to execute with the provided fixture.
+    */
     homePage: async ({page}, use) => {
         const homePage = new HomePage(page);
         await use(homePage);
     },
+    /**
+     * Provides an instance of RegistrationPage for tests.
+     * @param page - The Playwright page instance.
+     * @param use - The function to execute with the provided fixture.
+    */
+    registrationPage: async ({page}, use) => {
+        const registrationPage = new RegistrationPage(page);
+        await use(registrationPage);
+    },
+    /**
+     * Provides an instance of DigitalDownloadsPage for tests.
+     * @param page - The Playwright page instance.
+     * @param use - The function to execute with the provided fixture.
+    */
+    digitalDownloadsPage: async ({page}, use) => {
+        const digitalDownloadsPage = new DigitalDownloadsPage(page);
+        await use(digitalDownloadsPage);
+    },
+    /**
+     * Provides an instance of ShoppingCartPage for tests.
+     * @param page - The Playwright page instance.
+     * @param use - The function to execute with the provided fixture.
+    */
+    shoppingCartPage: async ({page}, use) => {
+        const shoppingCartPage = new ShoppingCartPage(page);
+        await use(shoppingCartPage);
+    },
+
 })
--- tests/addItemToShoppingCart.spec.ts
+++ tests/addItemToShoppingCart.spec.ts
@@ -1,17 +1,12 @@
 import { test } from '../fixture';
-import { DigitalDownloadsPage } from '../pages/digitalDownloadsPage';
-import { RegistrationPage } from '../pages/registrationPage';
-import { ShoppingCartPage } from '../pages/shoppingCartPage';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/');
 });
 
 test.describe('add item to shopping cart', () => {
-    test('add item to shopping cart for a new user', async ({ page, homePage }) => {
-        const registrationPage = new RegistrationPage(page);
-        const digitalDownloadsPage = new DigitalDownloadsPage(page);
-        const shoppingCartPage = new ShoppingCartPage(page);
+    test('add item to shopping cart for a new user', async ({ homePage, registrationPage, 
+        digitalDownloadsPage, shoppingCartPage }) => {
         await homePage.clickRegistration();
         await registrationPage.fillPersonalDetails();
         await registrationPage.fillPassword();
